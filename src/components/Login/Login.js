7f3e3db5f73960d6dcbb645565882fdc2a1812c9
shagors <shajjadshagor@gmail.com>
2022-04-09 17:35:03 +0600

    sign in lock page without login dont entered a page set private route

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,26 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
 import app from '../../firebase.init';
 
 const  auth = getAuth(app);
 const Login = () => {
     const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location?.state?.from?.pathname || '/';
     
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+        .then( () => {
+            navigate(from, {replace: true});
+        })
+    }
     return (
         <div>
             <h2 className='text-3xl my-3'>Please Login</h2>
-            <button onClick={() => signInWithGoogle()} className='mb-3 border-2 bg-blue-300 p-1'>Google Sign In</button>
+            <button onClick={handleGoogleSignIn} className='mb-3 border-2 bg-blue-300 p-1'>Google Sign In</button>
             <form>
                 <input className='border-2 mb-3' type="email" placeholder='Your Email' />
                 <br />
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h2 className='text-3xl my-3'>This is orders page</h2>
+        </div>
+    );
+};
+
+export default Orders;
