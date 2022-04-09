bf1e2b874576426839d97e999ad1abfba3d4ac58
shagors <shajjadshagor@gmail.com>
2022-04-09 10:48:43 +0600

    google signin provider add and login and logout button set

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h2 className='text-3xl my-3'>Please Login</h2>
-            <button className='mb-3 border-2 bg-blue-300 p-1'>Google Sign In</button>
+            <button onClick={signInWithGoogle} className='mb-3 border-2 bg-blue-300 p-1'>Google Sign In</button>
             <form>
                 <input className='border-2 mb-3' type="email" placeholder='Your Email' />
                 <br />
