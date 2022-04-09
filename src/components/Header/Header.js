329676c0fa4488b3952134791df90e142b5500f3
shagors <shajjadshagor@gmail.com>
2022-04-09 16:46:42 +0600

    use react firebase hook and connect with hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import app from '../../firebase.init';
 import './Header.css'
 
+const auth = getAuth(app);
 const Header = () => {
-    const {user, handleSignOut} = useFirebase();
+    const [user] = useAuthState(auth);
     return (
         <div className='header'>
             <nav>
@@ -12,12 +15,12 @@ const Header = () => {
                 <Link to='/products'>Products</Link>
                 <Link to='/orders'>Orders</Link>
                 <Link to='/register'>Register</Link>
-                <span>
+                <span className='mr-3'>
                     {user?.displayName && user.displayName}
                 </span>
                 {
                     user?.uid ? 
-                    <button onClick={handleSignOut} className='border-2 p-1'>Signout</button> 
+                    <button onClick={ () => signOut(auth)} className='border-2 p-1'>Signout</button> 
                     :
                     <Link to='/login'>Login</Link>}
             </nav>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
 
-const auth =getAuth(app);
+const auth = getAuth(app);
 
-const Home = () => {
+const Products = () => {
     const [user] = useAuthState(auth);
     return (
         <div>
-            <h2 className='text-3xl my-4'>Home Page</h2>
-            <p>Current user id : {user ? user?.displayName : 'No One Hear'}</p>
+            <h2 className='text-3xl my-3'>Knock Knock !!! Who Is There ??</h2>
+            <h5 className='text-2xl'>{user ? user?.displayName : 'Voooot Is hear'}</h5>
         </div>
     );
 };
 
-export default Home;
+export default Products;
